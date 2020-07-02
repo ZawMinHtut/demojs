c5537afa0112b6aedefd4a81453ece4eebcf84c0
Zaw Min Htut <kolattkl1996@gmail.com>
2020-07-02 17:02:06 +0630

    Version 1

--- core.js
+++ core.js
@@ -1,5 +1,6 @@
 (function() {
 	document.addEventListener("DOMContentLoaded", function(){
+		// ========== products ==========
 		let products = [
 		{
 			"name" : "pasta",
@@ -43,9 +44,9 @@
 		}
 		];
 
+		// ========== render products in DOM ==========
 		let productsDOM = document.querySelector(".products");
 
-		// render products in DOM
 		products.forEach(function(product, index) {
 			let div = document.createElement("div");
 			let p = document.createElement("p");
@@ -57,8 +58,9 @@
 				let li = document.createElement("li");
 				let size = document.createElement("span");
 				size.innerText = data.size;
+				size.style.textTransform = "capitalize";
 				let price = document.createElement("span");
-				price.innerText = data.price;
+				price.innerText = data.price + " MMK";
 				let button = document.createElement("button");
 				button.innerText = "Add To Card";
 				button.setAttribute("data-id", data.id);
@@ -75,7 +77,7 @@
 			}
 		});
 
-		//adding to card
+		// ========== adding to card ==========
 		let cardDOM = document.querySelector(".card");
 		let addButton = document.querySelectorAll(".products button");
 
@@ -97,18 +99,43 @@
 		function AddToCard(name, product) {
 			let card = JSON.parse(window.localStorage.getItem("card"));
 			if(card !== null) {
-				card.forEach(function(items) {
-					if(items[name].length) {
-						console.log("here");
+				for(let i = 0; i < card.length; i++) {
+					if(name in card[i]) {
+						for(let j = 0; j < card[i][name].length; j++) {
+							if(product.size in card[i][name][j]) {
+								card[i][name][j][product.size] = {
+									id: card[i][name][j][product.size].id,
+									qty: card[i][name][j][product.size].qty + 1,
+									price: card[i][name][j][product.size].price + card[i][name][j][product.size].price,
+								}
+							} else {
+								card[i][name][j][product.size] = {
+									id: product.id,
+									qty: 1,
+									price: product.price,
+								}
+							}
+						}
+					} else {
+						let tmp = [];
+						let item = new Object();
+						item[product.size] = {
+							id: product.id,
+							qty: 1,
+							price: product.price
+						}
+						tmp.push(item);
+						card[i][name] = tmp;
 					}
-				})
-				// console.log(JSON.stringify(card));
-				// window.localStorage.setItem("card", JSON.stringify(card));
+				}
+				
+				window.localStorage.setItem("card", JSON.stringify(card));
 			} else {
 				let data = new Object();
 				data[name] = [];
 				let item = new Object();
 				item[product.size] = {
+					id: product.id,
 					qty: 1,
 					price: product.price
 				}
@@ -116,16 +143,75 @@
 
 				window.localStorage.setItem("card", JSON.stringify([data]));
 			}
+
+			RenderCard();
 		}
 
-		//render card in DOM
-		// RenderCard();
+		// ========== render card in DOM ==========
+		let trToRender = document.querySelector(".card table tr:first-child");
+		let totalPrice = document.querySelector("#totalPrice");
+
+		RenderCard();
 
 		function RenderCard() {
+			let olderTrs = document.querySelectorAll(".card table tr:not(:first-child):not(:last-child)");
+			if(olderTrs.length) {
+				olderTrs.forEach(function(tr) {
+					tr.remove();
+				});
+			}
+			totalPrice.innerHTML = "0";
 			let card = JSON.parse(window.localStorage.getItem("card"));
 			if(card !== null) {
-				// console.log(card[0]);
+				card.forEach(function(item) {
+					let names = Object.keys(item);
+					names.forEach(function(name) {
+						let tr = document.createElement("tr");
+						let pName = document.createElement("td");
+						pName.innerText = name;
+						pName.style.textAlign = "center";
+						tr.append(pName);
+						let pSize = document.createElement("td");
+						let pQty = document.createElement("td");
+						pQty.style.textAlign = "center";
+						let pPrice = document.createElement("td");
+						pPrice.style.textAlign = "right";
+						item[name].forEach(function(data) {
+							let dataKeys = Object.keys(data);
+							dataKeys.forEach(function(key) {
+								if(pSize.innerHTML) {
+									pSize.innerHTML = pSize.innerHTML + "<br><hr>" + key;
+								} else {
+									pSize.innerHTML = key;
+								}
+
+								if(pQty.innerHTML) {
+									pQty.innerHTML = pQty.innerHTML + "<br><hr>" + data[key].qty;
+								} else {
+									pQty.innerHTML = data[key].qty;
+								}
+
+								if(pPrice.innerHTML) {
+									pPrice.innerHTML = pPrice.innerHTML + "<br><hr>" + data[key].price + " MMK";
+								} else {
+									pPrice.innerHTML = data[key].price + " MMK";
+								}
+								totalPrice.innerHTML = parseInt(totalPrice.innerHTML) + data[key].price;
+								tr.append(pSize, pQty, pPrice);
+							});
+						});
+						trToRender.parentNode.insertBefore(tr, trToRender.parentNode.lastElementChild);
+					});
+				});
 			}
 		}
+
+		// ========== clear card ==========
+		let clearCard = document.querySelector("#clearCard");
+		clearCard.addEventListener("click", function() {
+			window.localStorage.clear();
+			totalPrice.innerHTML = "0";
+			RenderCard();
+		})
 	});
 })();
--- index.php
+++ index.php
@@ -6,19 +6,28 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<div class="products"></div>
+	<main>
+		<div class="container">
+			<div class="products"></div>
 
-	<div class="card">
-		<h3>My Card</h3>
-		<table>
-			<tr>
-				<th>Products</th>
-				<th>Size</th>
-				<th>Qty</th>
-				<th>Price</th>
-			</tr>
-		</table>
-	</div>
+			<div class="card">
+				<h3>My Card</h3>
+				<table>
+					<tr>
+						<th>Products</th>
+						<th>Size</th>
+						<th>Qty</th>
+						<th>Price</th>
+					</tr>
+					<tr>
+						<td colspan="3">Total</td>
+						<td><span id="totalPrice">0</span> MMK</td>
+					</tr>
+				</table>
+				<button id="clearCard">Clear Card</button>
+			</div>
+		</div>
+	</main>
 
 	<script src="core.js"></script>
 </body>
